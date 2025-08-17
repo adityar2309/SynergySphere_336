2e16e3e915d915fa7a236caa1019e3d1f1753e30
GirishIITM <23f2005217@ds.study.iitm.ac.in>
2025-08-17 17:16:41 +0530

    removed unused files

--- frontend/src/components/admin-panel/sidebar.jsx
+++ frontend/src/components/admin-panel/sidebar.jsx
@@ -17,20 +17,23 @@ export function Sidebar() {
       className={cn(
         "fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
         !getOpenState() ? "w-[90px]" : "w-72",
-        settings.disabled && "hidden"
-      )}>
+        settings.disabled && "hidden",
+      )}
+    >
       <SidebarToggle isOpen={isOpen} setIsOpen={toggleOpen} />
       <div
         onMouseEnter={() => setIsHover(true)}
         onMouseLeave={() => setIsHover(false)}
-        className="relative h-full flex flex-col px-3 py-4 overflow-y-auto shadow-md dark:shadow-zinc-800">
+        className="side-bar-sect relative h-full flex flex-col px-3 py-4 overflow-y-auto shadow-md dark:shadow-zinc-800"
+      >
         <Button
           className={cn(
             "transition-transform ease-in-out duration-300 mb-1",
-            !getOpenState() ? "translate-x-1" : "translate-x-0"
+            !getOpenState() ? "translate-x-1" : "translate-x-0",
           )}
           variant="link"
-          asChild>
+          asChild
+        >
           <Link to="/dashboard" className="flex items-center gap-2">
             <img src={logo} width={100} alt="" />
           </Link>
